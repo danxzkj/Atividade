2e0cd26687b1f960193723459854fcbdd48eca4c
Daniel Casalli <danis.settk@gmail.com>
2025-05-16 09:42:10 -0300

    Prova2

---
+++ Prova 2/Ex1.js
@@ -0,0 +1,7 @@
+const valormesada = 150;
+let gastodomes = 75;
+let saldo = valormesada - gastodomes;
+
+console.log("Exercício 01");
+console.log("Valor restante: " + valormesada);
+console.log("-----------------------------")
