a7d25c662b4088fb969ce8ec00fef156f690651c
Daniel Casalli <danis.settk@gmail.com>
2025-05-16 08:17:10 -0300

    Ex3

---
+++ Prova1/Ex3.js
@@ -0,0 +1,16 @@
+let opção = 2;
+
+switch (opção) {
+    case 1:
+        console.log("Você escolheu cadastrar produto");
+        break
+    case 2:
+        console.log("Você escolheu listar o produtos");
+        break
+    case 3:
+        console.log("Você escolheu sair");
+        break
+    default:
+        console.log("Opção invalida")
+
+}
